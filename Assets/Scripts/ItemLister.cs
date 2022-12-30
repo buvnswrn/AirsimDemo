eda98c31e20616c4b15aa0a09fe071912252a589
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2022-12-30 21:40:36 +0100

    Added ItemLister

--- Assets/Scripts/ItemLister.cs
+++ Assets/Scripts/ItemLister.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,12 @@ public class ItemLister : MonoBehaviour
 {
     // Start is called before the first frame update
     public string objectOfInterest;
+
+    private List<GameObject> boxes;
+    private Dictionary<int, Dictionary<int, double>> distanceHeuristics = new Dictionary<int, Dictionary<int, double>>();
     void Start()
     {
-        List<GameObject> boxes = new List<GameObject>();
+         boxes = new List<GameObject>();
         var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
      foreach (var obj in allObjects)
      {
@@ -18,12 +22,39 @@ public class ItemLister : MonoBehaviour
          }
      }
 
-     foreach (var box in boxes)
-     {
-         Debug.Log(box.transform.position);
-     }
+     // foreach (var box in boxes)
+     // {
+     //     Debug.Log(box.transform.position);
+     // }
+     CalculateHeuristics();
     }
 
+    private void CalculateHeuristics()
+    {
+        foreach(GameObject box1 in boxes)
+        {
+            Dictionary<int, double> heuristicForThisBox = new Dictionary<int, double>();
+            foreach (GameObject box2 in boxes)
+            {
+                if (box1.GetInstanceID() != box2.GetInstanceID())
+                {
+                    double distance = getEuclideanDistance(box1.transform.position, box2.transform.position);
+                    Debug.Log(String.Format("Distance from box1({0}) - box2({1}) = {2}", box1.GetInstanceID(),
+                        box2.GetInstanceID(), distance));
+                    heuristicForThisBox.Add(box2.GetInstanceID(), distance);
+                }  
+            }
+            distanceHeuristics.Add(box1.GetInstanceID(),heuristicForThisBox);
+        }
+    }
+
+    public double getEuclideanDistance(Vector3 p1, Vector3 p2)
+    {
+        float dX = p2.x - p1.x;
+        float dY = p2.y - p1.y;
+        float dZ = p2.z - p1.z;
+        return Math.Sqrt(Math.Pow(dX, 2)+ Math.Pow(dY, 2) + Math.Pow(dZ, 2));
+    }
     // Update is called once per frame
     void Update()
     {
